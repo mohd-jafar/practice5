bb22a43258d6d9d34713271d1af93ec38c2a7a51
mohd-jafar <142247641+mohd-jafar@users.noreply.github.com>
2023-10-18 21:57:26 +0530

    practicing

---
+++ Operators.js
@@ -0,0 +1,14 @@
+console.log("calculator")
+
+var num1= 2
+var num2= 4
+
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1%num2)
+console.log(num1==num2)
+console.log(num1<num2)
+console.log(num1>num2)
+
+
