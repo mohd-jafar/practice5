2b4a1334bcff7e4e98714e2eb34e9fc94d4a7264
mohd-jafar <142247641+mohd-jafar@users.noreply.github.com>
2023-10-19 21:59:00 +0530

    str methods

---
+++ string.js
@@ -0,0 +1,29 @@
+var str= "Javascript"
+
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+
+var str1="Hello world"
+console.log(str1.slice(5,15))
+
+var str3= "Facebook, Google, Microsoft, Apple, IBM, Oracle,Amazon";
+console.log(str3.split(","))
+
+var str4="Javascript"
+console.log(str4.indexOf("a"))
+
+var str5="Javascript"
+console.log(str5.lastIndexOf("a"))
+
+
+var str6="Javascript is used"
+var str7=" to add functionalities to the web app"
+
+// console.log(str6+str7)
+console.log(str6.concat(str7))
+
+
+var str8="This course is all about Javascript"
+console.log(str8.includes("Javascript"))
+
+console.log(Math.random()*50);
