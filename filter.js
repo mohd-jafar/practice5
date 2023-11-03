9572c0be7fa03bb43dbfc372589a39023cd8f1db
mohd-jafar <142247641+mohd-jafar@users.noreply.github.com>
2023-11-03 21:56:57 +0530

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,17 @@
+console.log("FILTER");
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNumbers = numbers.filter((e) => e % 2 == 0);
+console.log(numbers);
+console.log("filter of even no.s",evenNumbers);
+
+
+var countries=['USA','Australia','dubai','newzealand']
+var countries5=countries.filter(e => e.length<6)
+console.log(countries);
+
+console.log("filtered countries",countries5);
+
+var countries1=['England',"Australia",""]
+
+
